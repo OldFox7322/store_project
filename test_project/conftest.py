26e53bb6f6395b45a372fb406c4de11d74bf0d92
Dmytro <dmytro.serdiuk73@gmail.com>
2025-08-16 17:33:21 +0200

    Update conftest.py

--- test_project/conftest.py
+++ test_project/conftest.py
@@ -16,7 +16,7 @@ import os
 load_dotenv()
 
 
-TEST_DATABASE_URL = os.getenv('TEST_DATABASE_URL')
+TEST_DATABASE_URL = os.getenv('TEST_DATABASE_URL').replace("postgresql://", "postgresql+asyncpg://", 1)
 
 
 
